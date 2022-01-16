d72c7773e4a966c267d55191b016b14d7ba8870d
Giuliano <giulioguidi10@gmail.com>
2022-01-16 19:58:33 -0300

    clase3-js

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/*let numero= 0
+
+while(numero < 6){
+
+    numero++
+    
+    alert(numero)
+}*/
+
+let nombre= prompt("ingrese su nombre")
+
+while(nombre != "giuliano"){
+    nombre= prompt("nombre erroneo, ingrese su nombre nuevamente");
+    if(nombre == "giuliano"){
+    alert("Hola Giuliano")}
+}
+
+/*let numero= 0
+do{
+    alert(numero)
+ 
+    numero++
+}
+while(numero > 6)*/
+
+/*for(let i= 0 ; i <= 10 ; i++){
+    alert(i)
+}*/
